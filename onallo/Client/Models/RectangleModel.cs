a86cbfb9c3298f1a072df1ff1106bd74c8b89f4e
victerial <mnandi2002@gmail.com>
2023-09-30 20:24:06 +0200

    Onallo_feladat_kesz

---
+++ onallo/Client/Models/RectangleModel.cs
@@ -0,0 +1,9 @@
+﻿namespace onallo.Client.Models
+{
+    public class RectangleModel
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Area => Width * Height;
+    }
+}
---
+++ onallo/Client/ViewModels/BaseClass/RectangleViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using onallo.Client.Models;
+
+
+namespace onallo.Client.ViewModels.BaseClass
+{
+    public class RectangleViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private RectangleModel _rectangle = new RectangleModel();
+
+        public double Width
+        {
+            get => _rectangle.Width;
+            set
+            {
+                if (_rectangle.Width != value)
+                {
+                    _rectangle.Width = value;
+                    OnPropertyChanged(nameof(Width));
+                    OnPropertyChanged(nameof(Area));
+                }
+            }
+        }
+
+        public double Height
+        {
+            get => _rectangle.Height;
+            set
+            {
+                if (_rectangle.Height != value)
+                {
+                    _rectangle.Height = value;
+                    OnPropertyChanged(nameof(Height));
+                    OnPropertyChanged(nameof(Area));
+                }
+            }
+        }
+
+        public double Area => _rectangle.Area;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
